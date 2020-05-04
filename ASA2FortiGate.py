3d82c4c35f6730ee09f45f5f04e3974839bbf6ef
Pavel Glushkov <heupomaht@gmail.com>
2020-05-05 01:56:11 +0300

    SecondCommit

--- ASA2FortiGate.py
+++ ASA2FortiGate.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""Converts ASA firewall configuration into compatible FortiGate"""
 
-print("It`s alive! Alive!")
+
+import sys
+import os
+
+if len(sys.argv)==1:
+    print("Usage: ASA2FortiGate.py <asaconfig>.txt [fgateconfig.txt]")
+    exit()
+
+
