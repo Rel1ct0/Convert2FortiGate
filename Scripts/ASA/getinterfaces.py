110786b2bd11ef2133090a6a81e7fdbf28e93ee0
Pavel Glushkov <heupomaht@gmail.com>
2020-05-12 22:48:08 +0300

    Added Red/Eth interfaces

--- Scripts/ASA/getinterfaces.py
+++ Scripts/ASA/getinterfaces.py
@@ -10,6 +10,13 @@ def getinterfaces(DATA) -> dict:
         if data[0] == "interface":
             interface_detected = True
             interface_name = data[1]
+            if interface_name.count('.'):
+                interface_params["type"] = 'subinterface'
+                interface_params["parent"] = interface_name.split('.')[0]
+            elif interface_name.startswith('Redundant'):
+                interface_params["type"] = 'redundant'
+            else:
+                interface_params["type"] = 'physical'
             # print("Interface detected", data[1])
         if interface_detected:
             if data[0] == "!":
@@ -32,4 +39,12 @@ def getinterfaces(DATA) -> dict:
                     interface_params["netmask"] = data[3]
                 if data[0] == "shutdown":
                     interface_params["shutdown"] = True
+                if data[0] == "channel-group":
+                    interface_params["type"] = 'portchannel-member'
+                    interface_params["parent"] = "Port-channel" + data[1]
+                    interface_params["port-channel-type"] = data[3]
+                if data[0] == "member-interface":
+                    if not interface_params.get("children"):
+                        interface_params["children"] = set()
+                    interface_params["children"].add(data[1])
     return result
