1c6e72571f97b1d97b6f2fa11ec36f3060f5788a
Pavel <heupomaht@gmail.com>
2020-06-04 17:05:19 +0300

    Started Named ACLs

--- Scripts/IOS/ACLs/getnamed.py
+++ Scripts/IOS/ACLs/getnamed.py
@@ -1,3 +1,16 @@
 def getnamed(DATA) -> dict:
     result = dict()
+    for _ in DATA:
+        data = _.strip().split()
+        if len(data) == 0:
+            continue
+        if data[0] == "ip" and data[1] == "access-list":
+            if data[2] == "standard":
+                if not result.get(data[3]):  # First line of ACL, create it
+                    result[data[3]] = list()
+                asdsa
+            elif data[2] == "extended":
+                if not result.get(data[3]):  # First line of ACL, create it
+                    result[data[3]] = list()
+                dasddas
     return result
--- Scripts/IOS/ACLs/getnumbered.py
+++ Scripts/IOS/ACLs/getnumbered.py
@@ -13,24 +13,26 @@ def getnumbered(DATA) -> dict:
                     if data[2] == 'remark':
                         ACE['comments'] = '"' + ' '.join(map(str, data[3:])) + '"'
                     else:  # Permit or deny
+                        ACE['src'] = dict()
                         if data[2] == 'permit':
                             ACE['action'] = 'allow'
                         else:
                             ACE['action'] = 'deny'
                         if data[3] == 'any':
-                            ACE['srcip'] = 'any'
+                            ACE['src']['ip'] = 'any'
                         elif data[3] == 'host':
-                            ACE['srcip'] = data[4]
-                            ACE['srcmask'] = '255.255.255.255'
+                            ACE['src']['ip'] = data[4]
+                            ACE['src']['mask'] = '255.255.255.255'
                         else:
-                            ACE['srcip'] = data[3]
+                            ACE['src']['ip'] = data[3]
                             if len(data) == 5:  # Wildcard mask defined
                                 wildmask = data[4].split('.')
                                 mask = [str(255 - int(x)) for x in wildmask]
-                                ACE['srcmask'] = '.'.join(mask)
+                                ACE['src']['mask'] = '.'.join(mask)
                             else:  # Wildcard mask not defined
-                                ACE['srcmask'] = '255.255.255.255'
-                        ACE['dstip'] = 'any'
+                                ACE['src']['mask'] = '255.255.255.255'
+                        ACE['dst'] = dict()
+                        ACE['dst']['ip'] = 'any'
                         ACE['proto'] = 'ip'
                     result[data[1]].append(ACE)
                 if (100 <= int(data[1]) < 200) or (2000 <= int(data[1]) < 2700):  # Extended ACL
@@ -46,48 +48,50 @@ def getnumbered(DATA) -> dict:
                         else:
                             ACE['action'] = 'deny'
                         ACE['proto'] = data[3]
+                        ACE['src'] = dict()
                         if data[4] == 'any':  # Source
-                            ACE['srcip'] = 'any'
+                            ACE['src']['ip'] = 'any'
                             data = data[5:]  # Cut off src
                         elif data[4] == 'host':
-                            ACE['srcip'] = data[5]
-                            ACE['srcmask'] = '255.255.255.255'
+                            ACE['src']['ip'] = data[5]
+                            ACE['src']['mask'] = '255.255.255.255'
                             data = data[5:]  # Cut off src
                         else:
-                            ACE['srcip'] = data[4]
+                            ACE['src']['ip'] = data[4]
                             wildmask = data[5].split('.')
                             mask = [str(255 - int(x)) for x in wildmask]
-                            ACE['srcmask'] = '.'.join(mask)
+                            ACE['src']['mask'] = '.'.join(mask)
                             data = data[6:]  # Cut off src
                         if data[0] in ['eq', 'gt', 'lt', 'neq']:  # Is source port defined?
-                            ACE['srcport'] = data[1]
-                            ACE['srcport_compare'] = data[0]
+                            ACE['src']['srcport'] = data[1]
+                            ACE['src']['srcport_compare'] = data[0]
                             data = data[2:]
                         elif data[0] == 'range':
-                            ACE['srcport'] = data[1]
-                            ACE['srcport_end'] = data[2]
-                            ACE['srcport_compare'] = data[0]
+                            ACE['src']['srcport'] = data[1]
+                            ACE['src']['srcport_end'] = data[2]
+                            ACE['src']['port_compare'] = data[0]
                             data = data[3:]
+                        ACE['dst'] = dict()
                         if data[0] == 'any':  # Destination
-                            ACE['dstip'] = 'any'
+                            ACE['dst']['ip'] = 'any'
                             data = data[1:]  # Cut off src
                         elif data[0] == 'host':
-                            ACE['dstip'] = data[1]
-                            ACE['dstmask'] = '255.255.255.255'
+                            ACE['dst']['ip'] = data[1]
+                            ACE['dst']['mask'] = '255.255.255.255'
                             data = data[3:]  # Cut off src
                         else:
-                            ACE['dstip'] = data[0]
+                            ACE['dst']['ip'] = data[0]
                             wildmask = data[1].split('.')
                             mask = [str(255 - int(x)) for x in wildmask]
-                            ACE['dstmask'] = '.'.join(mask)
+                            ACE['dst']['mask'] = '.'.join(mask)
                             data = data[2:]  # Cut off src
                         if data:  # Dst port can be absent
                             if data[0] in ['eq', 'gt', 'lt', 'neq']:
-                                ACE['dstport'] = data[1]
-                                ACE['dstport_compare'] = data[0]
+                                ACE['dst']['dstport'] = data[1]
+                                ACE['dst']['dstport_compare'] = data[0]
                             elif data[0] == 'range':
-                                ACE['dstport'] = data[1]
-                                ACE['dstport_end'] = data[2]
-                                ACE['dstport_compare'] = data[0]
+                                ACE['dst']['dstport'] = data[1]
+                                ACE['dst']['dstport_end'] = data[2]
+                                ACE['dst']['dstport_compare'] = data[0]
                     result[ACLName].append(ACE)
     return result
