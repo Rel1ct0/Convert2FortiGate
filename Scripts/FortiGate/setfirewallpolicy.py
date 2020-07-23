3145e4d56532d9b3ea81f16aa6914200779f34bd
pa.glushkov <heupomaht@gmail.com>
2020-07-23 15:02:10 +0300

    Rule numbering and "name" tweaks

--- Scripts/FortiGate/setfirewallpolicy.py
+++ Scripts/FortiGate/setfirewallpolicy.py
@@ -1,12 +1,13 @@
 def setfirewallpolicy(DATA: dict) -> str:
     result = 'config firewall policy\n'
     brk = ' ' * 4
-    rule_counter = 0
+    rule_counter = 10
     for interface, iface_params in DATA.get('interfaces').items():
         if not DATA['interface_map'].get(interface):  # Skipped interface, ignore
             continue
         if acl := iface_params.get('acl-in'):  # This interface has ACL applied
             comment = str()
+            acl_counter = 1
             for ace in DATA['acls'][acl]:  # Next ACE
                 if ace.get('comments'):  # This ACE is a comment
                     comment = ace.get('comments')
@@ -15,6 +16,7 @@ def setfirewallpolicy(DATA: dict) -> str:
                 if comment:  # Last ACE was a comment? Append it
                     result = result + brk*2 + 'set comments "' + comment.strip('"') + '"\n'
                     comment = str()
+                result = result + brk * 2 + 'set name "' + DATA['interface_map'][interface] + '_' + str(acl_counter) + '"\n'
                 result = result + brk * 2 + 'set srcintf ' + DATA['interface_map'][interface] + '\n'
                 result = result + brk * 2 + 'set dstintf any\n'
                 result = result + brk * 2 + 'set srcaddr ' + ace['src']['object'] + '\n'
@@ -24,7 +26,8 @@ def setfirewallpolicy(DATA: dict) -> str:
                 result = result + brk * 2 + 'set service ' + ace['service']['object'] + '\n'
                 result = result + brk * 2 + 'set schedule always\n'
                 result = result + brk + 'next\n'
-                rule_counter += 1
+                rule_counter += 10
+                acl_counter += 1
         else:  # Interface does not have ACL applied
             result = result + brk + 'edit ' + str(rule_counter) + '\n'
             result = result + brk * 2 + 'set srcintf ' + DATA['interface_map'][interface] + '\n'
@@ -35,6 +38,6 @@ def setfirewallpolicy(DATA: dict) -> str:
             result = result + brk * 2 + 'set dstaddr all\n'
             result = result + brk * 2 + 'set schedule always\n'
             result = result + brk + 'next\n'
-            rule_counter += 1
+            rule_counter += 10
     result = result + 'end\n'
     return result
